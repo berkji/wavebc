a50a3f072b30db4861299defd9b95a6ee0b3a6a3
Hans Petter Langtangen <hpl@simula.no>
2014-05-27 18:08:56 +0200

    updates

--- doc/src/wavebc/make.sh
+++ doc/src/wavebc/make.sh
@@ -8,17 +8,16 @@ mkdir $dest
 cp -r fig* mov* $dest
 
 doconce pygmentize wavebc.do.txt perldoc
-cp wavebc.do.txt.html $dest
+mv -f wavebc.do.txt.html $dest
 
 # Sphinx
-themes="cbc fenics_minimal"
+themes="cbc fenics_minimal1"
 for theme in $themes; do
 doconce format sphinx $name
-theme=cbc
 doconce sphinx_dir author='H. P. Langtangen' theme=$theme dirname=sphinx-$theme $name
 doconce split_rst $name
 python automake_sphinx.py
-cp -r sphinx-$theme $dest
+mv -f sphinx-$theme $dest
 done
 
 
@@ -32,25 +31,21 @@ doconce split_html ${name}_cyborg
 doconce format html $name --html_output=${name}_journal --html_style=bootswatch_journal
 doconce split_html ${name}_journal
 
-cp *.html ._*.html $dest
+mv -f *.html ._*.html $dest
 
 # PDF
 doconce format pdflatex $name --device=paper
-doconce ptex2tex $name envir=minted
-pdflatex -shell-escape $name
+doconce ptex2tex $name envir=minted  # use more printer friendly verb envir!
 pdflatex -shell-escape $name
 makeindex $name
 pdflatex -shell-escape $name
-pdflatex -shell-escape $name
-cp $name.pdf $dest/${name}_4print.pdf
+mv -f $name.pdf $dest/${name}_4print.pdf
 
 doconce format pdflatex $name --device=screen
 doconce ptex2tex $name envir=minted
 pdflatex -shell-escape $name
-pdflatex -shell-escape $name
 makeindex $name
 pdflatex -shell-escape $name
-pdflatex -shell-escape $name
-cp $name.pdf $dest/${name}.pdf
+mv -f $name.pdf $dest/${name}.pdf
 
 # Can now publish $dest directory
